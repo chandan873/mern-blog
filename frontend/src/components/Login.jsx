c86b8b612afe5f9d0ef1c4868139b5396ba7a951
Chandan singh <76685431+chandan873@users.noreply.github.com>
2025-03-01 02:53:22 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault();
     setError('');
     try {
-      const response = await fetch('http://localhost:4000/login', {
+      const response = await fetch('https://mern-blog-2-1ko4.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
